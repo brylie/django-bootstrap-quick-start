8f60e1de3254907a31ad6c201a74eba775712bd4
Brylie Christopher Oxley <brylie@amble.fi>
2021-12-23 14:45:46 +0200

    Register user model with Django admin

--- project/accounts/admin.py
+++ project/accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import User
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = User
+    list_display = ['email', 'username',]
